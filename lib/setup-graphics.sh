f53f771ccf22ff51704b1b46f2ca907f2b730722
uekyorf <uekyorf@gmail.com>
2019-06-03 22:13:15 +0900

    Add: NvidiaGraphicsDriversをインストールするスクリプトを追加

---
+++ lib/setup-graphics.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install NvidiaGraphicsDrivers for Debian9
+sudo bash -c "echo 'deb http://httpredir.debian.org/debian/ stretch main contrib non-free' >> /etc/apt/sources.list"
+
+sudo apt update
+sudo apt install linux-headers-$(uname -r|sed 's/[^-]*-[^-]*-//') nvidia-driver
