bce7545e0ddfa8cf1e618aa1865fc7c59deb29f8
uekyorf <uekyorf@gmail.com>
2019-11-19 15:16:14 +0900

    Add: gitのエイリアスを追加

--- lib/init.sh
+++ lib/init.sh
@@ -6,3 +6,12 @@
 for item in $HOME/bin/* ; do
   chmod u+x "$item"
 done
+
+xmodmap -pke > default.xmodmap
+
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.ch checkout
+git config --global alias.ad add
+git config --global alias.cm commit
+git config --global alias.re reset
