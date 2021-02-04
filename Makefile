2e9bc62d813a1f6facab801086812c324b6c1b74
shokkunrf <19404989+shokkunrf@users.noreply.github.com>
2021-02-04 21:59:06 +0900

    Revert "Change: インストール手順をMakefileに変更"
    This reverts commit 2e68afdeca01ca00ad10442f340578701394f140.

--- Makefile
+++
@@ -1,8 +0,0 @@
-DOTFILES := $(subst ./src/, , $(wildcard ./src/.??*))
-FILES := $(subst ./src/, , $(wildcard ./src/*))
-
-.PHONY: deploy
-deploy:
-	@$(foreach file, $(DOTFILES), ln -s $(abspath ./src/$(file)) $(HOME)/$(file);)
-	@$(foreach file, $(FILES), ln -s $(abspath ./src/$(file)) $(HOME)/$(file);)
-
---
+++ lib/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# 
+# Create symbolick link
+
+#########################
+# Create symbolick link
+# Globals:
+#   None
+# Arguments:
+#   DOT_DIRECTORY
+# Return:
+#   None
+#########################
+symbolink(){
+  local readonly DOT_DIRECTORY=`dirname $0`/..
+  local readonly LINK_LIST=(
+    '.profile'
+    '.bash_aliases'
+    '.bashrc'
+    '.Xmodmap'
+    '.Xresources'
+    '.vimrc'
+    'bin'
+    '.xmonad'
+  )
+
+  for file in "${LINK_LIST[@]}"; do
+    ln -s "${DOT_DIRECTORY}/${file}" "${HOME}/"
+  done
+}
+symbolink
+
+ln -s $HOME/.local/share/Trash/files $HOME/.Trash
