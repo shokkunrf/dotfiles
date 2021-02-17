3483b22ace9439b54a1018642b755f09ef0b47b9
shokkunrf <19404989+shokkunrf@users.noreply.github.com>
2021-02-17 22:32:38 +0900

    vscodeのインストールを修正

--- lib/install_bin.sh
+++ lib/install_bin.sh
@@ -43,11 +43,12 @@ install() {
     rm ./chrome.deb
   fi
   if $(is_contained 'vscode'); then
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > ./microsoft.gpg
+    sudo install -o root -g root -m 644 ./microsoft.gpg /etc/apt/trusted.gpg.d/
     sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
     sudo apt update
     sudo apt install code
+    rm ./microsoft.gpg
   fi
   if $(is_contained 'nvidia-driver'); then
     # Install NvidiaGraphicsDrivers for Debian9
