5d5a21e048a50b2ee3200e5a6529a79d66efaecf
shokkunrf <19404989+shokkunrf@users.noreply.github.com>
2021-02-18 18:59:30 +0900

    dockerのインストールを修正

--- lib/install_bin.sh
+++ lib/install_bin.sh
@@ -33,7 +33,7 @@ install() {
       containerd.io
     # docker-compose
     tag=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/docker/compose/releases/latest \
-      | jq '.name')
+      | jq -r '.name')
     sudo curl -L https://github.com/docker/compose/releases/download/$tag/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
   fi
