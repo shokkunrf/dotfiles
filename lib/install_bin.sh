5bfad0fcfaddbc1596cd5631f0740c6e91ed545d
shokkunrf <19404989+shokkunrf@users.noreply.github.com>
2021-02-18 19:30:39 +0900

    dockerグループへのユーザ追加を追加

--- lib/install_bin.sh
+++ lib/install_bin.sh
@@ -36,6 +36,9 @@ install() {
       | jq -r '.name')
     sudo curl -L https://github.com/docker/compose/releases/download/$tag/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
+
+    sudo gpasswd -a $USER docker
+    echo 'Please re-login'
   fi
   if $(is_contained 'chrome'); then
     wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
